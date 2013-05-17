8b1d78dcc0f56b5b4ec971f285491ab84074d9ff
Nacho <nacho@sezion.com>
2013-05-17 15:52:53 +0200

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var YoutubeUpload = require ('../lib/YoutubeUpload');
+
+var filePath            = '/var/tmp/video.mp4';
+var title               = 'title';
+var description         = 'description';
+var category            = 'Music';
+var keywords            = 'music';
+var googleAccessToken   = '';
+var youtubeDeveloperKey = '';
+
+YoutubeUpload (filePath, title, description, category, keywords, access_token, youtubeDeveloperKey, function (err, youtube_id) {
+
+  if (err) console.log ("Got error: "+err);
+  else     console.log ("Got id: "+youtube_id);
+
+});
